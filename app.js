095a813d20ea42f0d418a3f8ab50b8b17872da48
unknown <nathaliecortz@hotmail.com>
2019-03-15 14:12:44 -0600

    Se actualizó la data.

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 
+
+const data={
+  "series": [
+    {
+      "Actors": "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving",
+      "Awards": "Won 4 Oscars. Another 34 wins & 48 nominations.",
+      "BoxOffice": "N/A",
+      "Country": "USA",
+      "DVD": "21 Sep 1999",
+      "Director": "Lana Wachowski, Lilly Wachowski",
+      "Genre": "Action, Sci-Fi",
+      "Language": "English",
+      "Metascore": "73",
+      "Plot": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+      "Production": "Warner Bros. Pictures",
+      "Rated": "R",
+      "Ratings": [
+        { "Source": "Internet Movie Database",
+        "Value": "8.7/10"
+        }],
+      "Released": "31 Mar 1999",
+      "Response": "True",
+      "Runtime": "136 min",
+      "Title": "The Matrix",
+      "Type": "movie",
+      "Website": "http://www.whatisthematrix.com",
+      "Writer": "Lilly Wachowski, Lana Wachowski",
+      "Year": "1999",
+      "imdbID": "tt0133093"
+    }
+  ]
+}
+
 let movies = [];
 const allMovies= ["tt0133093", "tt0234215", "tt0242653", "tt0120783"];
 
-for (let i=0; i< allMovies.length; i++){
-  fetch ("http://www.omdbapi.com/?i=" + allMovies[i] +"&apikey=6b29b7cf" )
+/*for (let i=0; i< allMovies.length; i++){
+  fetch ("./data.json" )
   .then((data) =>{ return data.json();
   }).then((DataAsJSON) => {
     movies[i]=DataAsJSON;
     //Traducción de la data newData
      
   });
-}
-
+}*/
 document.querySelector(".screenContainer").style.display = "none";
 document.querySelector(".redirectingScreen").style.display="none";
 document.querySelector(".list").style.display="none";
@@ -24,7 +57,7 @@ console.log(movies);
     //document.getElementById("showSearch").style.display="block";
     document.getElementById("searchScreen").style.display="none";
     document.querySelector(".screenContainer").style.display = "block";
-    let tvShow=findTitle (movies, "The Matrix");
+    let tvShow=findTitle (data.series, "The Matrix");
     //printSearchTitle(tvShow);
     displayMovies(tvShow);
   }
